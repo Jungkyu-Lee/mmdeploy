10a9fa3612b87fd2ee6a08e340d835e6ff934b07
Jungkyu-Lee <jglee@mycreatz.com>
2024-07-19 11:06:04 +0900

    max_finite_val=1e4,

--- configs/mmpose/pose-detection_simcc_onnxruntime-fp16_dynamic.py
+++ configs/mmpose/pose-detection_simcc_onnxruntime-fp16_dynamic.py
@@ -1,6 +1,21 @@
+# _base_ = [
+#     './pose-detection_static.py', '../_base_/backends/onnxruntime-fp16.py'
+# ]
+
 _base_ = [
-    './pose-detection_static.py', '../_base_/backends/onnxruntime-fp16.py'
+    './pose-detection_static.py'
 ]
+
+backend_config = dict(
+    type='onnxruntime',
+    precision='fp16',
+    common_config=dict(
+        # min_positive_val=1e-7,
+        # max_finite_val=1e4,
+        keep_io_types=False,
+        disable_shape_infer=False,
+        op_block_list=None,
+        node_block_list=None))
 
 onnx_config = dict(
     input_shape=[192, 256],
